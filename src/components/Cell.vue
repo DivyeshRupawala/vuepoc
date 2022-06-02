dc51f0d471b81a41a09a9e9bc9f06a8591a07a8d
Divyesh Rupawala <divyesh.rupawala@o9solutions.com>
2022-06-02 12:12:46 +0530

    first commit

---
+++ src/components/Cell.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="cell-wrapper" v-on:dblclick="toggle()">
+    <span v-show="columnName == 'Image'"><img :src="value"></span>
+    <span v-show="columnName != 'Image' && !editMode">{{value}}</span> 
+    <span v-show="editMode" > <input  v-model="value"> <button  v-on:click="save()"> Save</button> <button v-on:click="toggle()"> Cancel</button></span>      
+</div>
+</template>
+
+<script>
+export default {
+  name: "cell",
+  props: {
+    value: String,    
+    columnName: String    
+  },
+    data(){
+    return {      
+      editMode: false,
+    }
+  },
+  methods: {    
+    toggle: function() {
+        if (this.columnName == "FIRST_NAME" || this.columnName == "LAST_NAME" || this.columnName == "SALARY") {
+            this.editMode = !this.editMode;
+        }
+    },
+    save: function() {
+         console.log("save " + this.value);
+        this.editMode = !this.editMode;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.cell-wrapper {
+    font-weight: bold !important;
+    color: white !important;
+}
+</style>
---
+++ src/components/Row.vue
@@ -0,0 +1,40 @@
+<template>
+    <tr class="row-wrapper" v-for="entry in filteredData" v-on:click="onRowSelection(entry)">
+        <td v-for="key in columns">     
+            <cell :value="entry[key]" :columnName="key"></cell>
+        </td>    
+    </tr>
+</template>
+
+<script>
+import Cell from "./Cell.vue";
+
+export default {
+  name: "row",
+  props: {
+    columns: Array,    
+    filteredData: Object    
+  },
+    data(){
+    return {      
+      editMode: false,
+    }
+  },
+  methods: {    
+    onRowSelection: function(selectedRow) {
+        console.log("Selected Row");
+        console.log(selectedRow)
+    }    
+  },
+   components: {      
+    Cell   
+  }
+}
+</script>
+
+<style scoped>
+.cell-wrapper {
+    font-weight: bold !important;
+    color: white !important;
+}
+</style>
